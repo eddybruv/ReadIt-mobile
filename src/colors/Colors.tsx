92ba6ea73516cef94928370a374501b7663dc3a4
eddybruv <edwinajong@gmail.com>
2022-09-27 22:09:10 -0500

    added some app colors

---
+++ src/colors/Colors.tsx
@@ -0,0 +1,6 @@
+export const Primary = "#2A297D";
+export const PrimaryDark = "#1C1B52";
+export const Accent = "#FDBD2B";
+export const White = "white";
+export const Dark = "black";
+export const LightShade1 = "#F5F5F5";
