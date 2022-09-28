af7712654094fa5a61046eb5272051385c937dff
eddybruv <edwinajong@gmail.com>
2022-09-27 22:09:35 -0500

    started with layout and screens

---
+++ src/layouts/LayoutWithoutNav.tsx
@@ -0,0 +1,14 @@
+import { View, StyleSheet } from "react-native";
+
+const LayoutWithoutNav = () => {
+  <View style={styles.body}></View>;
+};
+
+const styles = StyleSheet.create({
+  body: {
+    height: "100%",
+    width: "100%",
+  },
+});
+
+export default LayoutWithoutNav;
