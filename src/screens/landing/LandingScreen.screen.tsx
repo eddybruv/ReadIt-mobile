e77d6fce9f66d9b4de0c0a9a8c5c5c35219d1132
eddybruv <edwinajong@gmail.com>
2022-09-28 21:47:14 -0500

    added some mark up

--- src/screens/landing/LandingScreen.screen.tsx
+++ src/screens/landing/LandingScreen.screen.tsx
@@ -1,2 +1,17 @@
 import React from "react";
-import { View } from "react-native";
+import { Image, View } from "react-native";
+import LayoutWithoutNav from "../../layouts/LayoutWithoutNav";
+
+const LandingScreen = () => {
+  return (
+    <LayoutWithoutNav>
+      <View>Edwin</View>
+      <Image
+        source={require("../../images/image-1.png")}
+        style={{ height: "50%" }}
+      />
+    </LayoutWithoutNav>
+  );
+};
+
+export default LandingScreen;
